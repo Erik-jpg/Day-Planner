df9fdef60b55c4740c1b75242b49b8641bc7ef09
Erik Stone <spologas@gmail.com>
2021-06-28 17:40:55 -0700

    almost have it, just need to add setItem, gettItem, and have elements change based on time

--- javascript.js
+++ javascript.js
@@ -7,28 +7,67 @@
 //can start with an empty array that is then filled in with the save, ['','','','','','','','','','','','','','','','']
 //0 = 9 am, 1 = 10, ect
 //need eventListner for saving events
-//need a way to update when an event is saved 
+//need a way to update when an event is saved
 //display saved event for the day/hour
 //use an if statement to determine if they are first time user
 //find a way to load previous events and allow the user to add to the list
 const nowMoment = moment();
-const nowDate = new Date;
+const nowDate = new Date();
 const eDisplayMoment = document.querySelector("#displayMoment");
 const eDisplayDate = document.querySelector("#displayDate");
-const hourBlock = document.querySelector("#time-block");
- 
-function instantiateMoment(){
-    eDisplayMoment.innerHTML = moment().format('MMMM Do YYYY, hh:mm:ss a');
+const clock = document.querySelector(".time-block");
+const hourBlockElement = document.querySelector(".textarea");
+const saveB = document.querySelector(".saveBtn");
+const hourBlock = document.querySelectorAll(".hour");
+
+
+function instantiateMoment() {
+  eDisplayMoment.innerHTML = moment().format("MMMM Do YYYY, HH:mm:ss");
 }
 
-setInterval (() => {
-    instantiateMoment();
+setInterval(() => {
+  instantiateMoment();
 }, 1000);
 
- if (moment()!== nowMoment) {
-    hourBlock.classList.add(".past");
+
+
+//would a forEach be the way? it seems slow and clunky
+
+
+
+
+    
+// console.log(saveBtn);
+// }
+
+function handleTime (hourBlockElement,hourBlock) {
+    let thisHour = moment().format("HH");
+for (let i = 0; i < 9; i++) {
+    if (parseInt(hourBlockElement[i]) < parseInt(thisHour)) {
+        clock[i].classList.add(".past");
+      } else if (parseInt(hourBlockElement[i])=== parseInt(thisHour)) {
+        clock.classList.add(".present");
+      } else (parseInt(hourBlockElement[i]) > parseInt(thisHour)) 
+        hourBlock.classList.add(".future");
+    }
 }
 
-else if (moment()=== nowMoment) {
-    hourBlock.classList.add(".present");
-}
+
+handleTime();
+
+
+
+    function checkCalendar(localStorage) {
+        localStorage.getItem(hourBlock , hourBlockElement),value = hourBlockElement;
+        console.log(localStorage);
+    }
+  
+    //     let schedule = JSON.stringify(localStorage.setItem(nowMoment, hourBlockElement.value)) || [];
+    
+    //     let scheduledEvent = JSON.parse(localStorage.getItem("schedule")) || [];
+
+    //     const hourBlockElement = $(".textarea");
+
+    //     const colorByHour = document.querySelector(hourBlockElement[i].id)
+
+    // $(hourBlockElement[i].id).removeClass('.present .past .future')}}}
